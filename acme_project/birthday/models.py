ae0e23dc1e1a356889606f58b57db39f4b6d4e0c
Дарья Анохина <destedora-69@mail.ru>
2024-02-06 00:07:31 +0300

    add 6 commit

--- acme_project/birthday/models.py
+++ acme_project/birthday/models.py
@@ -1,9 +1,31 @@
 from django.db import models
+from django.urls import reverse
+from django.contrib.auth import get_user_model
+
+from .validators import real_age
+
+User = get_user_model()
 
 
 class Birthday(models.Model):
     first_name = models.CharField('Имя', max_length=20)
     last_name = models.CharField(
-        'Фамилия', blank=True, help_text='Необязательное поле', max_length=20
+        'Фамилия', help_text='Необязательное поле', max_length=20, blank=True
     )
-    birthday = models.DateField('Дата рождения')
+    birthday = models.DateField('Дата рождения', validators=(real_age,))
+    image = models.ImageField('Фото', upload_to='birthdays_images', blank=True)
+    author = models.ForeignKey(
+        User, verbose_name='Автор записи', on_delete=models.CASCADE, null=True
+    )
+
+    class Meta:
+        constraints = (
+            models.UniqueConstraint(
+                fields=('first_name', 'last_name', 'birthday'),
+                name='Unique person constraint',
+            ),
+        )
+
+    def get_absolute_url(self):
+        # С помощью функции reverse() возвращаем URL объекта.
+        return reverse('birthday:detail', kwargs={'pk': self.pk})
--- acme_project/birthday/urls.py
+++ acme_project/birthday/urls.py
@@ -1,12 +1,13 @@
-from django.urls import path
-
-from . import views
-
-app_name = 'birthday'
+# Импортируем настройки проекта.
+from django.conf import settings
+# Импортируем функцию, позволяющую серверу разработки отдавать файлы.
+from django.conf.urls.static import static
+from django.contrib import admin
+from django.urls import include, path
 
 urlpatterns = [
-    path('', views.birthday, name='create'),
-    path('list/', views.birthday_list, name='list'),
-    path('<int:pk>/edit/', views.birthday, name='edit'),
-    path('<int:pk>/delete/', views.delete_birthday, name='delete'),
-]
+    path('', include('pages.urls')),
+    path('admin/', admin.site.urls),
+    path('birthday/', include('birthday.urls')),
+    # В конце добавляем к списку вызов функции static.
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
